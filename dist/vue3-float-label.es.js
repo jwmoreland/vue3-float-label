11997337cc7774d937c0c679396c78540932195c
Justin Moreland <justin@brandhoot.com>
2026-06-15 17:47:04 -0500

    chore: bump version to 1.0.15 and upgrade to vite 8 to remediate esbuild RCE (GHSA-gv7w-rqvm-qjhr)

--- dist/vue3-float-label.es.js
+++ dist/vue3-float-label.es.js
@@ -1,73 +1,77 @@
-import { ref as t, computed as m, watch as I, onMounted as N, onBeforeUnmount as M, openBlock as v, createElementBlock as d, normalizeClass as g, renderSlot as W, unref as u, createElementVNode as x, toDisplayString as E, createCommentVNode as q, createTextVNode as A } from "vue";
-const H = {
-  key: 0,
-  class: "float-label__label float-label--no-click"
-}, R = { class: "float-label__label__text" }, z = ["for"], D = {
-  key: 0,
-  class: "float-label__label__bg"
-}, O = { class: "float-label__label__text" }, P = "[type=date], [type=datetime-local], [type=datetime], [type=email], [type=month], [type=number], [type=password], [type=search], [type=tel], [type=text], [type=time], [type=url], [type=week], textarea, select", $ = {
-  __name: "FloatLabel",
-  props: {
-    label: { type: String, default: "" },
-    float: { type: Boolean, default: null },
-    onFocus: { type: Boolean, default: !1 }
-  },
-  setup(F) {
-    const s = F;
-    let b = t(null), c = t(""), r = t(""), k = t(null), e = t({}), n = t(""), a = t(""), i = t(!1), p = t(!1), _ = t(!1);
-    const y = m(() => s.label), L = m(() => a.value === "select" ? !0 : r.value && c.value !== r.value), w = m(() => {
-      let l = !1;
-      return s.float ? l = !0 : l = i.value && i.value !== "0", l;
-    }), h = () => {
-      c.value = y.value ? s.label : r.value;
-    }, C = () => e.value.getAttribute("id"), T = () => ("10000000-1000-4000-8000" + -1e11).replace(/[018]/g, (o) => (o ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> o / 4).toString(16)), V = () => {
-      switch (a.value) {
-        case "input":
-        case "textarea":
-          return e.value.placeholder;
-        case "select":
-          let l = e.value.querySelector("option:disabled");
-          return l ? l.innerHTML : "";
-        default:
-          return "";
-      }
-    }, S = () => {
-      n.value || (n.value = T(), e.value.id = n.value);
-    }, B = () => {
-      a.value === "select" ? e.value.addEventListener("change", f) : e.value.addEventListener("input", f);
-    }, U = () => {
-      a.value === "select" ? e.value.removeEventListener("change", f) : e.value.removeEventListener("input", f);
-    }, f = (l) => {
-      i.value = l.target.value.length > 0;
-    };
-    return I(y, (l, o) => {
-      h();
-    }), N(() => {
-      k.value = s.label, e.value = b.value.querySelector(P), p.value = !!e.value, _.value = s.float === null, p.value ? (n.value = C(), a.value = e.value ? e.value.tagName.toLowerCase() : "", i.value = !!e.value.value, r.value = V(), S(), _.value === !0 && setTimeout(() => {
-        B();
-      }, 200)) : r.value = "", h();
-    }), M(() => {
-      p.value && U();
-    }), (l, o) => (v(), d("div", {
-      class: g(["float-label", { "float-label--fade-anim": L.value, "float-label--on-focus": s.onFocus, "float-label--fixed": w.value }]),
-      ref_key: "root",
-      ref: b
-    }, [
-      W(l.$slots, "default"),
-      u(a) === "" ? (v(), d("div", H, [
-        x("span", R, E(u(c)), 1)
-      ])) : (v(), d("label", {
-        key: 1,
-        class: g(["float-label__label", { "float-label--no-click": u(a) === "select" }]),
-        for: u(n)
-      }, [
-        u(a) === "textarea" ? (v(), d("span", D)) : q("", !0),
-        o[0] || (o[0] = A()),
-        x("span", O, E(u(c)), 1)
-      ], 10, z))
-    ], 2));
-  }
+import { computed as e, createCommentVNode as t, createElementBlock as n, createElementVNode as r, createTextVNode as i, normalizeClass as a, onBeforeUnmount as o, onMounted as s, openBlock as c, ref as l, renderSlot as u, toDisplayString as d, unref as f, watch as p } from "vue";
+//#region src/components/FloatLabel.vue
+var m = {
+	key: 0,
+	class: "float-label__label float-label--no-click"
+}, h = { class: "float-label__label__text" }, g = ["for"], _ = {
+	key: 0,
+	class: "float-label__label__bg"
+}, v = { class: "float-label__label__text" }, y = "[type=date], [type=datetime-local], [type=datetime], [type=email], [type=month], [type=number], [type=password], [type=search], [type=tel], [type=text], [type=time], [type=url], [type=week], textarea, select", b = {
+	__name: "FloatLabel",
+	props: {
+		label: {
+			type: String,
+			default: ""
+		},
+		float: {
+			type: Boolean,
+			default: null
+		},
+		onFocus: {
+			type: Boolean,
+			default: !1
+		}
+	},
+	setup(b) {
+		let x = b, S = l(null), C = l(""), w = l(""), T = l(null), E = l({}), D = l(""), O = l(""), k = l(!1), A = l(!1), j = l(!1), M = e(() => x.label), N = e(() => O.value === "select" ? !0 : w.value && C.value !== w.value), P = e(() => {
+			let e = !1;
+			return e = x.float ? !0 : k.value && k.value !== "0", e;
+		}), F = () => {
+			C.value = M.value ? x.label : w.value;
+		}, I = () => E.value.getAttribute("id"), L = () => "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (e) => (e ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> e / 4).toString(16)), R = () => {
+			switch (O.value) {
+				case "input":
+				case "textarea": return E.value.placeholder;
+				case "select":
+					let e = E.value.querySelector("option:disabled");
+					return e ? e.innerHTML : "";
+				default: return "";
+			}
+		}, z = () => {
+			D.value || (D.value = L(), E.value.id = D.value);
+		}, B = () => {
+			O.value === "select" ? E.value.addEventListener("change", H) : E.value.addEventListener("input", H);
+		}, V = () => {
+			O.value === "select" ? E.value.removeEventListener("change", H) : E.value.removeEventListener("input", H);
+		}, H = (e) => {
+			k.value = e.target.value.length > 0;
+		};
+		return p(M, (e, t) => {
+			F();
+		}), s(() => {
+			T.value = x.label, E.value = S.value.querySelector(y), A.value = !!E.value, j.value = x.float === null, A.value ? (D.value = I(), O.value = E.value ? E.value.tagName.toLowerCase() : "", k.value = !!E.value.value, w.value = R(), z(), j.value === !0 && setTimeout(() => {
+				B();
+			}, 200)) : w.value = "", F();
+		}), o(() => {
+			A.value && V();
+		}), (e, o) => (c(), n("div", {
+			class: a(["float-label", {
+				"float-label--fade-anim": N.value,
+				"float-label--on-focus": x.onFocus,
+				"float-label--fixed": P.value
+			}]),
+			ref_key: "root",
+			ref: S
+		}, [u(e.$slots, "default"), f(O) === "" ? (c(), n("div", m, [r("span", h, d(f(C)), 1)])) : (c(), n("label", {
+			key: 1,
+			class: a(["float-label__label", { "float-label--no-click": f(O) === "select" }]),
+			for: f(D)
+		}, [
+			f(O) === "textarea" ? (c(), n("span", _)) : t("", !0),
+			o[0] ||= i(),
+			r("span", v, d(f(C)), 1)
+		], 10, g))], 2));
+	}
 };
-export {
-  $ as FloatLabel
-};
+//#endregion
+export { b as FloatLabel };
